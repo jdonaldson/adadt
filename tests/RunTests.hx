b0ffffe9c57faf9f17201ce5027db61b21dc7d00
Justin Donaldson <jdonaldson@gmail.com>
2018-05-17 09:15:17 -0700

    disable flash, fix nits

--- tests/RunTests.hx
+++ tests/RunTests.hx
@@ -8,8 +8,7 @@ class RunTests {
     Runner.run(TestBatch.make([
           new ApiTest()
     ])).handle(Runner.exit);
-    travix.Logger.println('it works');
-    travix.Logger.exit(0); // make sure we exit properly, which is necessary on some targets, e.g. flash & (phantom)js
+    travix.Logger.exit(0);
   }
 
 
